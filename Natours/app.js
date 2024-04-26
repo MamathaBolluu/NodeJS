4d6f043f4c87f666609d7d25f309a598421c91fd
MamathaBolluu <151727688+MamathaBolluu@users.noreply.github.com>
2024-04-26 11:14:10 +0530

    express routing project added

---
+++ Natours/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const morgan = require('morgan');
+
+
+const tourRouter = require('./routes/tourRoute');
+const userRouter  = require('./routes/userRoute')
+
+const app = express();
+
+//  1 MIDDLEWARE
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req,res,next)=>{
+    console.log("Hello  from the middleware");
+    next();
+})
+
+app.use((req,res,next)=>{
+    req.requestTime  = new Data().toISOString();
+    next();
+})
+ 
+// ROUTES
+app.use('/api/v1/tours',tourRouter);
+app.use('/api/v1/users',userRouter);
+
+
+module.exports = app;
+
+
+
+
+
